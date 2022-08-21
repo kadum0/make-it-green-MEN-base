e12dadb2fd66b8c1f66fc20d9bd0b442045f299a
kadum <75564464+kadum2@users.noreply.github.com>
2022-08-21 22:00:29 +0300

    get data and deploy them

--- public/home.js
+++ public/home.js
@@ -40,19 +40,13 @@ const map = L.map('map').setView([33.396600, 44.356579], 9); //leaflet basic map
 
 /////get the home data; map statics 
 window.onload= async ()=>{
+
+    // get map statics
     let fetchedMapStatics = await fetch("/mapStatics")
     console.log(fetchedMapStatics)
     let mapStatics = await fetchedMapStatics.json()
     console.log(mapStatics)
 
-// [...mapStatics].foreach(e=>{console})
-
-// console.log(mapStatics.red.length)
-// mapStatics.green
-
-    Object.values(mapStatics).forEach(e=>{
-        console.log(e)
-    })
     //insert red
     Object.values(mapStatics.red).forEach(e=>{
         // L.marker(e.coords)
@@ -80,7 +74,6 @@ window.onload= async ()=>{
     //     }).bindPopup(`<h3> ❤المساهمين</h3> ${e.aNames}`).addTo(map)
     // })
 
-
     document.querySelector("#greenPinCounter").textContent = mapStatics.green.length
     document.querySelector("#redPinCounter").textContent = mapStatics.red.length
 
@@ -102,9 +95,70 @@ window.onload= async ()=>{
     
 
 
-    // insert pins 
+
+    // get articles 
+    let getarticles = await fetch('/articles')
+    let articles = await getarticles.json()
+    console.log(articles)
+
+    // deploy artciles; 
+        // make objects; imgs 
+
+        let articleTemp =  `
+        <div class="article">
+        <img style='background:url("${articles[0].img}")'> 
+        <div class="content">
+            <h2>${articles[0].title}</h2>
+            <p>${articles[0].content}</p>
+        </div>
+    </div>
+
+    <div class="article">
+    <img style='background:url("${articles[1].img}")'> 
+    <div class="content">
+        <h2>${articles[1].title}</h2>
+        <p>${articles[1].content}</p>
+    </div>
+</div>
+
+<div class="article">
+<img style='background:url("${articles[2].img}")'> 
+<div class="content">
+    <h2>${articles[2].title}</h2>
+    <p>${articles[2].content}</p>
+</div>
+</div>
+    `
+    document.querySelector('.article-samples').innerHTML = articleTemp
 
 
-    
+    // get donors
+    let getDonors = await fetch('/donors')
+    let donors = await getDonors.json()
+    console.log(donors)
+    console.log(donors[0].logo)
+
+    ///deploy donors 
+        ///make elements
+
+    let donorsTemp = `
+        <div class="donor">
+            <img style='background:url("${donors[0].logo}")'> 
+            <h2 class="donor-name">${donors[0].donorName}</h2>
+        </div>
+
+        <div class="donor">
+            <img style='background:url("${donors[1].logo}")'> 
+            <h2 class="donor-name">${donors[1].donorName}</h2>
+        </div>
+        <div class="donor">
+            <img style='background:url("${donors[2].logo}")'> 
+            <h2 class="donor-name">${donors[2].donorName}</h2>
+        </div>
+    `
+        ////insert elements
+    document.querySelector('.donor-samples').innerHTML = donorsTemp
+
+
 }
 
